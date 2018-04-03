fa4805ec0822dd9b53ce6f5ad742c27d17b165de
Lorenzo Antonio Brito Morales <lorenzo.brito@corp.nearshoretechnology.com>
2018-04-03 10:06:46 -0500

    epplus sample

---
+++ TextEpplus/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TextEpplus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataSourceBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.column1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column2DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column3DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column4DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.column5DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSourceBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(330, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataSourceBindingSource
+            // 
+            this.dataSourceBindingSource.DataSource = typeof(TextEpplus.Form1.DataSource);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.column1DataGridViewTextBoxColumn,
+            this.column2DataGridViewTextBoxColumn,
+            this.column3DataGridViewTextBoxColumn,
+            this.column4DataGridViewTextBoxColumn,
+            this.column5DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.dataSourceBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(554, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // column1DataGridViewTextBoxColumn
+            // 
+            this.column1DataGridViewTextBoxColumn.DataPropertyName = "Column1";
+            this.column1DataGridViewTextBoxColumn.HeaderText = "Column1";
+            this.column1DataGridViewTextBoxColumn.Name = "column1DataGridViewTextBoxColumn";
+            // 
+            // column2DataGridViewTextBoxColumn
+            // 
+            this.column2DataGridViewTextBoxColumn.DataPropertyName = "Column2";
+            this.column2DataGridViewTextBoxColumn.HeaderText = "Column2";
+            this.column2DataGridViewTextBoxColumn.Name = "column2DataGridViewTextBoxColumn";
+            // 
+            // column3DataGridViewTextBoxColumn
+            // 
+            this.column3DataGridViewTextBoxColumn.DataPropertyName = "Column3";
+            this.column3DataGridViewTextBoxColumn.HeaderText = "Column3";
+            this.column3DataGridViewTextBoxColumn.Name = "column3DataGridViewTextBoxColumn";
+            // 
+            // column4DataGridViewTextBoxColumn
+            // 
+            this.column4DataGridViewTextBoxColumn.DataPropertyName = "Column4";
+            this.column4DataGridViewTextBoxColumn.HeaderText = "Column4";
+            this.column4DataGridViewTextBoxColumn.Name = "column4DataGridViewTextBoxColumn";
+            // 
+            // column5DataGridViewTextBoxColumn
+            // 
+            this.column5DataGridViewTextBoxColumn.DataPropertyName = "Column5";
+            this.column5DataGridViewTextBoxColumn.HeaderText = "Column5";
+            this.column5DataGridViewTextBoxColumn.Name = "column5DataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSourceBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource dataSourceBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column2DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column3DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column4DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn column5DataGridViewTextBoxColumn;
+    }
+}
+
---
+++ TextEpplus/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using System.IO;
+using OfficeOpenXml;
+
+namespace TextEpplus
+{
+    public partial class Form1 : Form
+    {
+      public  class DataSource
+        {
+            public string Column1 { get; set; }
+            public string Column2 { get; set; }
+            public string Column3 { get; set; }
+            public string Column4 { get; set; }
+            public string Column5 { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            dataSource_ = new DataSource();
+          
+        }
+        public DataSource dataSource_;
+        public List<DataSource> dataSource
+        {
+            get;
+            set;
+           
+    }
+            
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var lista = this.dataSourceBindingSource.List ;
+            int fila = 5;
+            char column = 'B';
+        
+            string FilePath = @"C:\Users\lorenzo.brito\Documents\test.xlsx";
+           
+            Console.WriteLine();
+
+            FileInfo existingFile = new FileInfo(FilePath);
+            Utils.OutputDir = new DirectoryInfo(@"C:\Users\lorenzo.brito\Documents");
+            FileInfo newFile = Utils.GetFileInfo("newfilesample.xlsx");
+           // FileInfo templateFile = Utils.GetFileInfo(FilePath, false);
+            using (ExcelPackage package = new ExcelPackage(newFile, existingFile))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets["Sheet1"];
+                foreach (DataSource item in lista)
+                {
+                    var x = item;
+                    
+                        worksheet.Cells[column++.ToString() + fila.ToString()].Value = item.Column1;
+                        worksheet.Cells[column++.ToString() + fila.ToString()].Value = item.Column2;
+                        worksheet.Cells[column++.ToString() + fila.ToString()].Value = item.Column3;
+                        worksheet.Cells[column++.ToString() + fila.ToString()].Value = item.Column4;
+                        worksheet.Cells[column++.ToString() + fila.ToString()].Value = item.Column5;
+                   
+                    column = 'B';
+                    fila++;
+                }
+                package.Save();
+            }
+        }
+    }
+}
